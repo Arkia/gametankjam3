df57da620f3f0cd9fcd31c368a4f526e0a05fc81
Arkia <arkia64@gmail.com>
2023-12-16 18:31:36 -0500

    ACP attempt 1

--- main.s
+++ main.s
@@ -1,15 +1,62 @@
 .INCLUDE "gametank_cpu.i"
+.INCLUDE "acp/acp.i"
 
 .ROMBANKSIZE $4000
 .ROMBANKS 2
 
-.BANK 1 SLOT 4
-
+.SECTION "MainProg" BANK 1 SLOT 4
 reset:
   ; Init Code
   stz BANK_FLAGS  ; Reset bank settings
   lda #%01001001  ; Enable blitter and colorfill mode
   sta DMA_FLAGS   ; Setup blitter
+  
+  ; Load ACP program
+  ldx 0                 ; Data index
+-
+  lda acp_prog,x        ; Load next program byte
+  sta ACP_PROG_START,x  ; Store into audio memory
+  inx                   ; Increment index
+  cpx acp_size          ; All bytes copied?
+  bne -
+  
+  ldx 0
+-
+  lda acp_vectors
+  sta $3FFA,x
+  inx
+  cpx 6
+  bne -
+  
+  ldx VOICE_COUNT-1
+  lda 8
+-
+  sta VOICE_VOLUME,x
+  dex
+  bne -
+  
+  ldx 0
+  lda 0
+-
+  sta $3300,x
+  clc
+  adc #4
+  inx
+  cpx 64
+  bne -
+  
+  stz VOICE_WAVE_LO
+  lda $33
+  sta VOICE_WAVE_HI
+  stz VOICE_VOLUME
+  stz VOICE_STEP_LO
+  lda $01
+  sta VOICE_STEP_HI
+  
+  lda #1
+  sta AUDIO_RESET
+  lda #$FF
+  sta AUDIO_RATE
   
   lda #16
   sta DMA_VX
@@ -29,8 +76,17 @@ irq:
   
 nmi:
   rti
+.ENDS
+
+.SECTION "ACPImport"
+  acp_prog:
+    .INCBIN "acp/acp.dat" READ -6 FREADSIZE acp_size
+  acp_vectors:
+    .INCBIN "acp/acp.dat" SKIP acp_size
+.ENDS
   
-.ORGA $FFFA
+.SECTION "VectorTable" BANK 1 SLOT 4 ORGA $FFFA FORCE
   .DW nmi
   .DW reset
   .DW irq
+.ENDS
