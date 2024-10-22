1cb8bd4dbb9dc7c6b93cb64841919afc79fc4efe
Arkia <arkia64@gmail.com>
2024-10-22 18:32:51 -0400

    Drawing routines

--- main.s
+++ main.s
@@ -26,18 +26,7 @@
 .DEFINE DRAW_BUFFER_DOWN  $40
 
 .RAMSECTION "DrawEngine" BANK 0 SLOT 0
-  draw_status     db
-  draw_mode       db
-  draw_x          db
-  draw_y          db
-  draw_color      db
-  draw_data       dw
-  draw_read       dw
-  draw_write      dw
-.ENDS
-
-.RAMSECTION "DrawBuffer" BANK 0 SLOT 2 ALIGN 512
-  draw_buffer     dsb 512
+  draw_status db
 .ENDS
 
 .RAMSECTION "InterruptVars" BANK 0 SLOT 0
@@ -50,20 +39,12 @@ reset:
   cld
   sei
   stz DMA_FLAGS
+  stz dma_flags
   lda #%00111000  ; Clip draws and draw to frame 1
   sta BANK_FLAGS  ; Reset bank settings
   sta bank_flags  ; Update mirror
   
-  stz $0F
-  lda #$80
-  sta draw_mode
   stz draw_status
-  stz draw_read
-  stz draw_write
-  lda #>draw_buffer
-  sta draw_read+1
-  eor #1
-  sta draw_write+1
   
   lda #$7F        ; Disable ACP
   sta AUDIO_RATE
@@ -92,6 +73,7 @@ reset:
   stz AUDIO_RESET
   lda #$FF
   sta AUDIO_RATE
+  sta VIA_DDRB
   
   jsr enable_blitter
   ldx #$0
@@ -112,37 +94,27 @@ reset:
   sta dma_flags   ; Update mirror
   cli
   
-  ldy #0
--
-  lda test_draw.w,y
-  sta (draw_write),y
-  iny
-  cpy #3
-  bne -
-  
-  lda draw_status
-  ora #%01000000
-  sta draw_status
+  lda #~$04
+  jsr clear_screen
+  jsr display_flip
   
 main_loop:
   jsr update_input            ; Read controllers
   jsr wait_frame              ; Wait for VBLANK
   bra main_loop
   
-test_draw:
-  .DB $00 $FB                 ; Clear screen
-  .DB $07                     ; Halt
-  
 enable_sprite_ram:
-  lda DMA_FLAGS
+  lda dma_flags
   and #%11011110
   sta DMA_FLAGS
+  sta dma_flags
   rts
   
 enable_blitter:
-  lda DMA_FLAGS
+  lda dma_flags
   ora #%00000001
   sta DMA_FLAGS
+  sta dma_flags
   rts
   
 set_sprite_quadrant:
@@ -164,6 +136,49 @@ quadrant_gx_table:
 quadrant_gy_table:
   .DB 0, 0, 128, 128
   
+display_flip:
+  lda bank_flags              ; Get current bank state from mirror
+  eor #%00001000              ; Flip framebuffer target
+  sta BANK_FLAGS              ; Set bank state
+  sta bank_flags              ; Set mirror
+  lda dma_flags               ; Get current blitter state
+  eor #%00000010              ; Flip displayed framebuffer
+  sta DMA_FLAGS               ; Set blitter state
+  sta dma_flags               ; Set mirror
+  rts
+  
+clear_screen:
+  sta DMA_COLOR               ; Set draw color
+  lda dma_flags               ; Get current blitter flags
+  ora #%10001000              ; Set opaque and color fill mode
+  sta DMA_FLAGS               ; Set blitter flags
+  lda #64                     ; Rectangle size
+  sta DMA_WIDTH
+  sta DMA_HEIGHT
+  ldx #3                      ; 4 times
+-
+  lda clear_pos_x.w,x           ; Get X position of quadrant
+  sta DMA_VX                  ; Set blit X
+  lda clear_pos_y.w,x           ; Get Y position of quadrant
+  sta DMA_VY                  ; Set blit Y
+  lda #1                      ; Blit start command
+  sta DMA_START               ; Do blit
+  wai                         ; Wait for blitter
+  dex                         ; Decrement X
+  bpl -                       ; Loop
+  rts
+  
+clear_pos_x:
+  .DB 0, 64, 0, 64
+clear_pos_y:
+  .DB 0, 0, 64, 64
+  
+wait_blitter:
+  bbr0 draw_status,+          ; Return if blitter is done
+  wai                         ; Wait for blitter
++
+  rts
+  
 wait_frame:
   lda frame_count             ; Load current frame counter
 -
@@ -171,14 +186,6 @@ wait_frame:
   cmp frame_count             ; Compare to frame counter
   beq -                       ; If counter hasn't changed, keep waiting
   rts                         ; Return
-  
-irq:
-  stz DMA_START               ; Clear interrupt
-  rti
-  
-nmi:
-  inc frame_count           ; Increment frame counter
-  rti
   
 update_input:
   lda GAMEPAD2            ; Reset latch on controller 1
@@ -208,6 +215,15 @@ update_input:
   cpx #2                  ; Last controller?
   bne -                   ; Loop
   rts                     ; Return
+  
+irq:
+  stz draw_status             ; Blitter finished
+  stz DMA_START               ; Clear interrupt
+  rti
+  
+nmi:
+  inc frame_count           ; Increment frame counter
+  rti
   
 .ENDS
 
